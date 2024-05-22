471c56c259fa3daed836e4dc832e0705dee94b57
saksit <saksit@alphametrics.co.th>
2024-05-22 22:45:59 +0700

    change back

--- src/main/java/com/betagroup/task/application/TaskService.java
+++ src/main/java/com/betagroup/task/application/TaskService.java
@@ -29,7 +29,7 @@ public class TaskService implements ITaskService {
 
     @Override
     public Ack delete(String id) {
-        return null;
+        return taskRepository.delete(id);
     }
 
     @Override
